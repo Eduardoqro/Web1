c4d3ef83002218208eced425cf2ccc2b6cb50734
Eduardoqro <eduardomendozaqro@gmail.com>
2022-09-12 12:40:38 -0500

    feat:proyecto buscar coincidencias

---
+++ prueba/miscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Todas las lineas que comiencen con gatito son comentarios
+echo"Se mostrará un mensaje en la linea de comandos"
+echo"Voy a crear un archivo"
+touch archivo.txt
+mkdir eduardo
+mkdir karla
+mkdir audrey
+mkdir alex
+touch eduardo/perfil.txt
+touch karla/perfil.txt
+echo"El usuario de esta sesión es:"
+whoami
+exit
+
